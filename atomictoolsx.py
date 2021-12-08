df82b4cccf1c3eacbacfb05b23ae248c79a2f07b
BonezIT <94773769+BonezIT@users.noreply.github.com>
2021-12-07 20:29:15 -0500

    Update atomictoolsx.py

--- atomictoolsx.py
+++ atomictoolsx.py
@@ -1,1 +1,18 @@
+"""Helpers for creating actions on atomictoolsx contract. By Vyryn, licenesed under AGPL."""
+from aioeos import types
 
+contract = 'atomictoolsx'
+
+
+def announcelink(creator: str, key: str, asset_ids: [int], memo: str, authorization=[]) -> types.EosAction:
+    return types.EosAction(
+        account=contract,
+        name='announcelink',
+        authorization=authorization,
+        data={
+            'creator': creator,
+            'key': key,
+            'asset_ids': asset_ids,
+            'memo': memo
+        }
+    )
